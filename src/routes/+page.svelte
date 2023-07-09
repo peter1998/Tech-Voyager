74eaa6f7faecfaf3875b43ced5084ab27b309c88
Peter Matov <pmatov@gmail.com>
2023-07-09 17:48:34 +0300

    [C] update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,39 +2,44 @@
 	// You can add JavaScript code here
 </script>
 
-<nav>
-	<a href="/">Home</a>
-	<a href="/about">About</a>
-	<a href="/projects">Projects</a>
-	<a href="/cv">CV</a>
-	<a href="/contact">Contact</a>
+<nav class="flex justify-around bg-gray-800 p-6 text-white">
+	<a href="/" class="hover:text-gray-300">Home</a>
+	<a href="/about" class="hover:text-gray-300">About</a>
+	<a href="/projects" class="hover:text-gray-300">Projects</a>
+	<a href="/cv" class="hover:text-gray-300">CV</a>
+	<a href="/contact" class="hover:text-gray-300">Contact</a>
 </nav>
 
-<section class="hero">
-	<h1>Hello, I'm Peter Matov</h1>
-	<p>
+<section class="hero text-center py-20">
+	<h1 class="text-4xl mb-4">Hello, I'm Peter Matov</h1>
+	<p class="text-xl mb-8">
 		I'm a software engineer and data scientist. Welcome to my personal website where you can learn
 		more about me, my projects, and my skills.
 	</p>
-	<a href="/projects" class="cta">View My Projects</a>
+	<a href="/projects" class="cta bg-gray-800 text-white py-2 px-4 rounded hover:bg-gray-700"
+		>View My Projects</a
+	>
 </section>
 
-<section class="skills">
-	<h2>My Skills</h2>
-	<ul>
-		<li>Software Engineering</li>
-		<li>Data Science</li>
+<section class="skills text-center py-20">
+	<h2 class="text-3xl mb-4">My Skills</h2>
+	<ul class="list-none p-0">
+		<li class="mb-2">Software Engineering</li>
+		<li class="mb-2">Data Science</li>
 		<!-- Add more skills here -->
 	</ul>
 </section>
 
-<section class="projects">
-	<h2>Recent Projects</h2>
+<section class="projects text-center py-20">
+	<h2 class="text-3xl mb-4">Recent Projects</h2>
 	<!-- We'll add code to display your projects here later -->
 </section>
 
-<section class="cta">
-	<h2>Interested in working together?</h2>
-	<a href="/contact" class="cta">Contact Me</a>
-	<a href="/cv" class="cta">View My CV</a>
+<section class="cta text-center py-20">
+	<h2 class="text-3xl mb-4">Interested in working together?</h2>
+	<a href="/contact" class="cta bg-gray-800 text-white py-2 px-4 rounded hover:bg-gray-700 mr-4"
+		>Contact Me</a
+	>
+	<a href="/cv" class="cta bg-gray-800 text-white py-2 px-4 rounded hover:bg-gray-700">View My CV</a
+	>
 </section>
