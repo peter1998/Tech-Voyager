85ed543234e5e2b40c7214e2a1c22dfb1fd1544d
Peter Matov <pmatov@gmail.com>
2023-07-09 16:47:08 +0300

    [C] Progress

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,14 +3,38 @@
 </script>
 
 <nav>
+	<a href="/">Home</a>
 	<a href="/about">About</a>
 	<a href="/projects">Projects</a>
 	<a href="/cv">CV</a>
 	<a href="/contact">Contact</a>
 </nav>
 
-<h1>Welcome to My Personal Website</h1>
-<p>
-	Hi, I'm Peter Matov. I'm a software engineer and data scientist. Welcome to my personal website
-	where you can learn more about me, my projects, and my skills.
-</p>
+<section class="hero">
+	<h1>Hello, I'm Peter Matov</h1>
+	<p>
+		I'm a software engineer and data scientist. Welcome to my personal website where you can learn
+		more about me, my projects, and my skills.
+	</p>
+	<a href="/projects" class="cta">View My Projects</a>
+</section>
+
+<section class="skills">
+	<h2>My Skills</h2>
+	<ul>
+		<li>Software Engineering</li>
+		<li>Data Science</li>
+		<!-- Add more skills here -->
+	</ul>
+</section>
+
+<section class="projects">
+	<h2>Recent Projects</h2>
+	<!-- We'll add code to display your projects here later -->
+</section>
+
+<section class="cta">
+	<h2>Interested in working together?</h2>
+	<a href="/contact" class="cta">Contact Me</a>
+	<a href="/cv" class="cta">View My CV</a>
+</section>
