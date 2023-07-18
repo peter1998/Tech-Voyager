e8242c80521aab06ea264098be5d3b662e07c63c
Peter Matov <pmatov@gmail.com>
2023-07-18 20:43:26 +0300

    [C] Netlify config

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,12 @@
+import netlify from '@sveltejs/adapter-netlify';
 import { vitePreprocess } from '@sveltejs/kit/vite';
-import vercel from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
 	preprocess: vitePreprocess(),
-
 	kit: {
-		adapter: vercel()
+		adapter: netlify(),
+		target: '#svelte'
 	}
 };
 
