16fd273c737f31b2f0fd3908a8761f806586a240
Peter Matov <pmatov@gmail.com>
2023-07-18 20:22:38 +0300

    vite config redo

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,6 @@ const config = {
 			allow: ['public']
 		}
 	},
-	assetsInclude: ['**/*.JPG', '**/*.jpg', '**/*.png', '**/*.svg', '**/*.jpeg'],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
