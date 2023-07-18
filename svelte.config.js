b80bfa1f5700f864dae5efefdf57fd9fc3185694
Peter Matov <pmatov@gmail.com>
2023-07-18 20:59:30 +0300

    [D] back to vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,14 @@
-import netlify from '@sveltejs/adapter-netlify';
 import { vitePreprocess } from '@sveltejs/kit/vite';
+import vercel from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
+	// for more information about preprocessors
 	preprocess: vitePreprocess(),
+
 	kit: {
-		adapter: netlify()
-		// target: '#svelte' // remove this line
+		adapter: vercel()
 	}
 };
 
